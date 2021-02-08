fe4fc2fe0d6029d5af2021a2cfe04bd39a2f8d2e
Balint Ligeti <balint.ligeti@hotmail.com>
2021-02-08 19:33:33 +0100

    Validate account number.

---
+++ src/components/AccNums.jsx
@@ -0,0 +1,65 @@
+import {React, useEffect, useState} from 'react'
+import Modulus from './Modulus'
+export default function AccNums(props) {
+
+    const codes = {
+        '0': ' _ ' +
+             '| |' +
+             '|_|',
+        '1': '   ' +
+             '  |' +
+             '  |',
+        '2': ' _ ' +
+             ' _|' +
+             '|_ ',
+        '3': ' _ ' +
+             ' _|' +
+             ' _|',
+        '4': '   ' +
+             '|_|' +
+             '  |',
+        '5': ' _ ' +
+             '|_ ' +
+             ' _|',
+        '6': ' _ ' +
+             '|_ ' +
+             '|_|',
+        '7': ' _ ' +
+             '  |' +
+             '  |',
+        '8': ' _ ' +
+             '|_|' +
+             '|_|',
+        '9': ' _ ' +
+             '|_|' +
+             ' _|'
+      }
+
+      const parseInputText = (file) => {
+        console.log(file)
+        const returnText = file;
+
+        const lenOfStr = returnText.substr(0, returnText.indexOf('\n')).length; 
+        const inputText = returnText.split("\n").join("");
+        const list = [inputText.slice(0,lenOfStr),inputText.slice(lenOfStr,lenOfStr * 2),inputText.slice(lenOfStr * 2,lenOfStr * 3)]
+      
+        let inputTextNumber = ""; 
+        
+        for (let i = 0; i < lenOfStr; i += 3) {
+           inputTextNumber += Object.keys(codes).find(key => codes[key] === 
+            list[0][i] + list[0][i+1] + list[0][i+2] +
+            list[1][i] + list[1][i+1] + list[1][i+2] +
+            list[2][i] + list[2][i+1] + list[2][i+2]);
+        }
+
+        return inputTextNumber;
+        
+      }
+
+    return (
+        <div>
+            <p>{parseInputText(props.text)}</p>
+            <Modulus stringInt={parseInputText(props.text)} />
+        </div>
+    )
+}
---
+++ src/components/Modulus.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Modulus(props) {
+    const calculateChecksum = (strNum) => {
+        const reversed = strNum.split('').reverse().join('');
+        let counter = 1;
+        let addNums = 0
+        reversed.split("").map((character) => {
+            console.log(character)
+            addNums += counter * parseInt(character);
+            counter++;
+        })
+        return addNums % 11 == 0 ? "Valid account number!" : "Invalid account number!";
+    }
+    return (
+        <div>
+            <p>{calculateChecksum(props.stringInt)}</p>
+        </div>
+    )
+}
