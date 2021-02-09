f3553ba9ab1411b049edff6e61cf3ccde71804e0
Balint Ligeti <balint.ligeti@hotmail.com>
2021-02-09 09:51:33 +0100

    half resolved revalidate num (doesn't work)

--- src/components/AccNums.jsx
+++ src/components/AccNums.jsx
@@ -65,9 +65,59 @@ export default function AccNums(props) {
             list[1][i] + list[1][i+1] + list[1][i+2] +
             list[2][i] + list[2][i+1] + list[2][i+2]);
         }
+
+        if (inputTextNumber.includes('undefined')) {
+             if (reCheck(list,lenOfStr) !== '') inputTextNumber = returnStr;
+        };
+
+
         const inputTextNumWithQuestioner = inputTextNumber.split("undefined").join("?");
         return setNum(inputTextNumWithQuestioner);
       }
+
+      let returnStr = '';
+     const reCheck = (list,lenOfStr) => {
+          for (let i = 0; i < lenOfStr; i += 3) {
+               if (codes.hasOwnProperty(list[0][i] + list[0][i+1] + list[0][i+2] +
+                    list[1][i] + list[1][i+1] + list[1][i+2] +
+                    list[2][i] + list[2][i+1] + list[2][i+2])){
+                         returnStr += list[0][i] + list[0][i+1] + list[0][i+2] +
+                         list[1][i] + list[1][i+1] + list[1][i+2] +
+                         list[2][i] + list[2][i+1] + list[2][i+2]
+                    }
+               else {
+                    checkByEveryChar(list[0][i] + list[0][i+1] + list[0][i+2] +
+                         list[1][i] + list[1][i+1] + list[1][i+2] +
+                         list[2][i] + list[2][i+1] + list[2][i+2])
+               }
+               return returnStr
+            }
+     }
+     let counter = 0;
+     const checkByEveryChar = (num) => {
+          let testStr = ''
+          let maybeGood = ''
+          for (let i = 0; i < num.length; i++) {
+               if (num[i] == '') {
+                    testStr = num;
+                    testStr[i] = '_';
+                    if (codes.hasOwnProperty(testStr)){
+                         counter += 1;
+                         maybeGood = testStr;
+                         testStr = ''
+                    } else if (codes.hasOwnProperty(testStr)){
+                         testStr[i] = '_'
+                         counter += 1;
+                         maybeGood = testStr;
+                         testStr = '';
+                    }
+               }
+          }
+               if (counter > 1) {
+                    return;
+               }
+               returnStr += codes.hasOwnProperty(testStr)
+     }
 
     return (
         <div>
